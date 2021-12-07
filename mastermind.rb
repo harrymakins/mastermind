77f98b6898852aa424490c8fd8219e9bd86cedaf
harry <harry.makins@gmail.com>
2021-12-07 11:41:24 +0000

    more sudo code

---
+++ mastermind.rb
@@ -0,0 +1,47 @@
+class Mastermind
+#start the game, choose how many games to play, must be an even number
+#computer randomly selects 4 pegs of 4 different colors
+#start loop, ask player to guess the colors, check against actuall colors, check turn, check win, give feedback based on game rules
+
+end
+
+class Player
+
+  def get_game_number
+
+  end
+  
+  def get_guess
+  
+  end
+
+end
+
+class Computer
+
+  def peg_selector
+
+  end
+
+  def guess_feedback
+
+  end
+
+end
+
+class Board
+
+  def check_turn
+  
+  end
+
+  def check_guess
+
+  end
+
+  def check_win
+
+  end
+end
+
+
